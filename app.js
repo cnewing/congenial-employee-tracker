8eb41ad1efc2fef5a0549e6b2c268f9ae94a3f49
Christa <christanewing47@gmail.com>
2022-08-27 19:51:22 -0400

    star structuring the server

---
+++ app.js
@@ -0,0 +1,6 @@
+// R E Q U I R E  D E P E N D E N C I E S
+const mysql = require("mysql");
+const consoleTable = require("console.table");
+const inquirer = require("inquirer");
+
+// P O R T  &  S E R V E R
