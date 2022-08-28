d38c271d7e0a1510a130684a207856893d1c2383
Christa <christanewing47@gmail.com>
2022-08-28 12:12:39 -0400

    update seeds

---
+++ db/Employeeseed.sql
@@ -0,0 +1,17 @@
+INSERT INTO department(department_name)
+VALUES
+    ("Marketing"), 
+    ("Engineering"), 
+    ("HR"), 
+    ("CS");
+
+INSERT INTO role(title, salary, department_id)
+VALUES
+    ("Email Developer",80000, 3), 
+    ("Graphic Designer",55000, 3), 
+    ("Senior Engineer", 120000, 2), 
+    ("Software Engineer",90000, 2), 
+    ("HR Manager", 75000, 1), 
+    ("Customer Service Representative", 45000, 4),
+    ("Customer Service Manager", 60000, 4),
+    ("HR Assistant", 50000, 1);
