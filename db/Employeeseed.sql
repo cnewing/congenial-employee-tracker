a15608cd1fd5b5d1b62ae269583578059b5c6328
Christa <christanewing47@gmail.com>
2022-08-28 12:23:52 -0400

    finish seeds

--- db/Employeeseed.sql
+++ db/Employeeseed.sql
@@ -1,3 +1,4 @@
+-- D E P A R T M E N T  N A M E S
 INSERT INTO department(department_name)
 VALUES
     ("Marketing"), 
@@ -5,6 +6,7 @@ VALUES
     ("HR"), 
     ("CS");
 
+-- R O L E S , S A L A R I E S , &  D E P T  I D 
 INSERT INTO role(title, salary, department_id)
 VALUES
     ("Email Developer",80000, 3), 
@@ -15,3 +17,15 @@ VALUES
     ("Customer Service Representative", 45000, 4),
     ("Customer Service Manager", 60000, 4),
     ("HR Assistant", 50000, 1);
+
+-- E M P L O Y E E S , R O L E S, &  M A N A G E R  I D
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES
+("Shawna", "Moore", 1, null),
+("Tiffany", "Newton", 1,),
+("Mars", "Blackmon", 4),
+("Greer", "Childs", 4,null),
+("Jamie", "Overstreet", 2, null),
+("Opal", "Gray", 2),
+("Christa", "Newing", 3)
+("Nola", "Darling", 3
